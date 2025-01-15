0861f2cbbd5ea72b1abaa41856ff585f9cb23272
Morlok <nikitaillarionov2018@gmail.com>
2025-01-15 18:51:35 +0200

    TL: 18.32 Initiated repo with next js, created starting envo for react, created Header component

---
+++ app/reactbookingapp/app/layout.jsx
@@ -0,0 +1,26 @@
+// import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+import "@/assets/styles/globals.css";
+import Header from "@/components/Header";
+
+const inter  = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Booking app | Book a Room",
+  description: "Book a meeting or conference room for you and your team",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={inter.className}
+      >
+        <Header />
+        <main className="main.mx-auto.max-w-7xl.px-4.py-6 sm:px-6 lg:px-8">
+        {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/reactbookingapp/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Booking app</h1>
+    </>
+  );
+}
